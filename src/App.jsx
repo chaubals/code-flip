23987cd8d336b68b2a94b3ef92c25da3393c3334
Siddhesh Chaubal <siddhesh.chaubal2024@gmail.com>
2024-10-27 19:48:27 -0400

    Configured AWS Amplify for hosting and Cognito for Authentication. Login page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import awsconfig from './aws-exports'; // Make sure this file is correctly generated
+import { Route, Router, Routes } from "react-router-dom";
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+Amplify.configure(awsconfig);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function MyApp() {
+    return (
+        <div>
+        <Authenticator>
+            {({ signOut, user }) => (
+                <div>
+                    <Routes>
+                        <Route path="/" element = {<HomePage/>} />
+                        <Route path="/flashcards/:topic" element= {<FlashcardBundle/>} />
+                    </Routes>
+                    <button className="button-signout" onClick={signOut}>Sign out</button>
+                </div>
+            )}
+        </Authenticator>
+        </div>
+    );
 }
-
-export default App
